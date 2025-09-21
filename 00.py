16137b415b47859103affc1dbf2803abb7f6ff0e
Hodgins Ohannigan <hodgins@192.168.1.105>
2025-09-21 14:23:10 -0500

    segundo commit

--- 00.py
+++ 00.py
@@ -3,3 +3,23 @@ esto es un comentario
 multilinea
 """
 
+'''
+Otro comentario
+multilinea
+'''
+
+first_variable = "string"
+
+first_variable = 50
+
+FIRST_CONSTANT = "Example"
+
+isItOn = True
+
+percentage = 24.9
+
+print("Hola Python", percentage)
+
+print(type(isItOn))
+
+print(type(percentage))
---
+++ 01.py
@@ -0,0 +1,14 @@
+'''
+Operadores
+'''
+
+edad = 5 + 5
+
+newEdad = edad * 3
+
+print(newEdad)
+
+print(f"Exponente: 2 ^ 3 = {2**3}")
+
+print(f"Modulo: 10 % 3 = {10%3}")
+
