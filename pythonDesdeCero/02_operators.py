fa12e44034e4af23df49f92ea0c51ad07b016206
Hodgins Ohannigan Dell <gjuanpablo746@gmail.com>
2025-09-30 18:13:52 -0500

    operadores

---
+++ pythonDesdeCero/02_operators.py
@@ -0,0 +1,40 @@
+#Arimeticos
+
+exponentiation = 2**2
+
+print(exponentiation)
+
+module = 5%2
+
+print(f"El modulo de 5 / 2 =",module)
+
+floor_division = 10//3
+
+print(f"El resultado de la division entera 10 / 3 es igual a", floor_division)
+
+division = 10/3
+
+print(f"Sin embargo la division normal seria igual a",division)
+
+
+print("Concatenando enteros y strings:", str(110))
+
+my_float = 2.5 * 2
+
+print("Formateando float como integer:", int(my_float))
+
+#Comparativos
+
+operador_comparativo = 5 >= 2
+
+print(operador_comparativo)
+
+#logicos
+
+print("##Operadores comparativos##")
+
+print( True and True )
+
+print( True or False )
+
+print( not False )
