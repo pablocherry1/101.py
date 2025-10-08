87e6f255c54a6f77f9470f9950017d548ab47728
Hodgins Ohannigan Dell <gjuanpablo746@gmail.com>
2025-10-08 17:39:00 -0500

    strings

--- pythonDesdeCero/03_strings.py
+++ pythonDesdeCero/03_strings.py
@@ -10,8 +10,16 @@ my_tab_string = "\tEste es un string con tabulacion"
 
 print(my_tab_string)
 
-name, surname, age = "JP", "Hodgins", 31
+name, surname, age = "juan pablo", "Hodgins", 31
 
-print("Linea con marcadores \nNombre: {} \nNickname: {} \nEdad: {}".format(name, surname, age))
+print("Linea con marcadores Nombre: {} Nickname: {} Edad: {}".format(name, surname, age))
 
-print("Linea con marcadores \nNombre: %s \nNickname: %s \nEdad: %d"%(name, surname, age))
+print("Linea con placeholders Nombre: %s Nickname: %s Edad: %d"%(name, surname, age))
+
+print(f"string literals o interpolacion Nombre: {name} Nickname: {surname} Edad: {age}")
+
+print(name.capitalize())
+
+print(name.upper())
+
+print(name.count("b"))
