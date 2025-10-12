81409e34bc701a3003ec0536285541bf22ac0ade
Hodgins Ohannigan <hodgins@Hodginss-MacBook-Pro.local>
2025-10-12 06:55:49 -0500

    listas

---
+++ pythonDesdeCero/04_lists.py
@@ -0,0 +1,15 @@
+### Lists ###
+
+my_list = list() 
+# Usa el constructor: 
+# es más lento y menos "Pythónico" (idiomático), pero útil si necesitas pasar un iterable para inicializar.
+my_other_list = []
+# Usa la sintaxis literal (corchetes): 
+# es la forma más rápida y preferida por la comunidad para crear una lista vacía.
+
+print(len(my_list))
+
+my_list.count(0) # Metodo de lista
+# Retorna el numero de ocurrencias de un valor
+
+len(my_list) # Funcion incorporada
