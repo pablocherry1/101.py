d39d8dcdf7ce412682b50457f18e3414f4682bea
Hodgins Ohannigan <gjuanpablo746@gmail.com>
2025-10-31 14:58:15 -0500

    primer cipas

--- POO/clases.py
+++ POO/clases.py
@@ -7,8 +7,25 @@ class Dog:
         self.age = age
         
     def __str__(self):
-        return f"Dog {self.name} breed {self.breed} and {self.age} years old"
+        return f"Dog {self.name}, breed {self.breed} and {self.age} years old"
     
-    # Create a dog
+    #Method
 
-    dog1 = Dog("Nemesis","Corgi",3)
+    def bark (self):
+        print(f"{self.name} is barking")
+
+    def play (self, toy):
+        print(f"{self.name} is playing with the {toy}")
+
+
+# Create a dog
+
+dog1 = Dog("Nemesis","Corgi",3)
+
+print(dog1)
+
+print(f"Nombre del cachorro: {dog1.name}")
+
+dog1.bark()
+
+dog1.play("bone")
