0dbab4baa542702a2b9f3a638a1258e8d9c93a53
Hodgins Ohannigan <hodgins@Hodginss-MacBook-Pro.local>
2026-02-01 04:44:37 -0500

    Inicio proyecto final

---
+++ 01.py
@@ -0,0 +1,19 @@
+'''
+Operadores
+'''
+
+edad = 5 + 5
+
+newEdad = edad * 3
+
+print(newEdad)
+
+print(f"Exponente: 2 ^ 3 = {2**3}")
+
+print(f"Modulo: 10 % 3 = {10%3}")
+
+print(True and True)
+
+print(False or True)
+
+print(not 10 + 3 == 14)
